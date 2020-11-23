f906f2b076c6e8ac46dd7efca873261deb6e046a
Roberto Cortez <radcortez@yahoo.com>
2020-11-23 18:52:52 +0000

    Add repository input parameter.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/smallrye/smallrye-config
+IFS='/' read -ra ORG_REPO <<< "$1"
+IFS='@' read -ra REPO_BRANCH <<< "${ORG_REPO[1]}"
 
-cd smallrye-config
+ORG=${ORG_REPO[0]}
+REPO=${REPO_BRANCH[0]}
+BRANCH=${REPO_BRANCH[1]}
+
+echo ${ORG}
+echo ${REPO}
+echo ${BRANCH}
+
+if [[ -z ${BRANCH} ]]; then
+    git clone https://github.com/${ORG}/${REPO}
+else
+    git clone --branch ${BRANCH} https://github.com/${ORG}/${REPO}
+fi
+
+cd ${REPO}
 
 mvn clean install -Dmaven.repo.local=${HOME}/.m2/repository
