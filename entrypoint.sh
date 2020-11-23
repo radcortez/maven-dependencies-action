3fb1c5ec332632b6798e190ecf83643ed242fec3
Roberto Cortez <radcortez@yahoo.com>
2020-11-23 23:54:42 +0000

    Support multiline input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-echo 'input' $1
-
-IFS=' ' read -ra REPOS <<< "$1"
+IFS=$'\n' read -d '' -a REPOS <<< "$1"
 
 for i in "${REPOS[@]}"
 do
-    echo 'i' ${i}
+    echo 'Building' ${i}
+
     IFS='/' read -ra ORG_REPO <<< "$i"
     IFS='@' read -ra REPO_BRANCH <<< "${ORG_REPO[1]}"
 
@@ -14,17 +13,13 @@ do
     REPO=${REPO_BRANCH[0]}
     BRANCH=${REPO_BRANCH[1]}
 
-    echo 'organization' ${ORG}
-    echo 'repository' ${REPO}
-    echo 'branch' ${BRANCH}
-done
+    if [[ -z ${BRANCH} ]]; then
+        git clone https://github.com/${ORG}/${REPO}
+    else
+        git clone --branch ${BRANCH} https://github.com/${ORG}/${REPO}
+    fi
 
-#if [[ -z ${BRANCH} ]]; then
-#    git clone https://github.com/${ORG}/${REPO}
-#else
-#    git clone --branch ${BRANCH} https://github.com/${ORG}/${REPO}
-#fi
-#
-#cd ${REPO}
-#
-#mvn clean install -Dmaven.repo.local=${HOME}/.m2/repository
+    cd ${REPO}
+
+    mvn install -DskipTests=true -Dmaven.repo.local=${HOME}/.m2/repository
+done
